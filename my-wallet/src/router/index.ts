089e661398e6064ebb977d2c212dd32a959dede7
Vidocq17 <avidanB1704@gmail.com>
2025-07-29 15:19:35 +0200

    refactor: simplify router configuration in index.ts

--- my-wallet/src/router/index.ts
+++ my-wallet/src/router/index.ts
@@ -1,15 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import App from '@/App.vue'
 
+const routes = [
+  { path: '/', name: 'home', component: App },
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: App,
-    },
-  ],
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
